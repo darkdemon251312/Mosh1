0e4a4edf43a2151a144d3acd5a9836b2680822f8
darkdemon251312 <44729472+darkdemon251312@users.noreply.github.com>
2018-11-15 13:16:30 -0500

    Tiempo
    reloj en cuenta regresiva mostrado en la pantalla

---
+++ Txt-Tiempo.gd
@@ -0,0 +1,9 @@
+extends Label
+
+func _ready():
+	set_physics_process(true)
+	
+func _physics_process(delta):
+	var Minutos = int(get_node("Tiempo").get_time_left() / 60) 
+	var Segundos = int(get_node("Tiempo").get_time_left()) % 60
+	set_text(str(Minutos) + ":" + str(Segundos))
